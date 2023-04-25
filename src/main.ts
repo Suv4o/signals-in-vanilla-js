e022962624d4b170a349f2b96f20e66160817c01
Aleks <aleksandar.trpkovski@gmail.com>
2023-04-25 14:56:43 +1000

    Signals in Vanilla JS

---
+++ src/main.ts
@@ -0,0 +1,77 @@
+import "./style.css";
+import { signal, computed, effect } from "@preact/signals-core";
+const counter = signal(1);
+const result = computed(() => counter.value * 10);
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1>Signals in Vanilla JS</h1>
+    <div class="card">
+      <button type="button">count is <span class="counter">${counter.value}</span></button>
+    </div>
+    <p>Result: <span class="counter">${counter.value}</span> * 10 = <span class="result">10</span></p>
+  </div>
+`;
+
+function setCounter() {
+    const counterElements = document.querySelectorAll<HTMLSpanElement>(".counter");
+    const resultElement = document.querySelector<HTMLSpanElement>(".result");
+
+    if (!counterElements?.length || !resultElement) {
+        return;
+    }
+
+    counterElements.forEach((element) => {
+        element.innerText = counter.value.toString();
+    });
+
+    resultElement.innerText = result.value.toString();
+}
+
+const button = document.querySelector<HTMLButtonElement>("button");
+
+button?.addEventListener("click", () => {
+    counter.value++;
+});
+
+effect(setCounter);
+
+// import "./style.css";
+// import { ref, computed, watch } from "vue";
+// const counter = ref(1);
+// const result = computed(() => counter.value * 10);
+
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+//   <div>
+//     <h1>Signals in Vanilla JS</h1>
+//     <div class="card">
+//       <button type="button">count is <span class="counter">${counter.value}</span></button>
+//     </div>
+//     <p>Result: <span class="counter">${counter.value}</span> * 10 = <span class="result">10</span></p>
+//   </div>
+// `;
+
+// function setCounter() {
+//     const counterElements = document.querySelectorAll<HTMLSpanElement>(".counter");
+//     const resultElement = document.querySelector<HTMLSpanElement>(".result");
+
+//     if (!counterElements?.length || !resultElement) {
+//         return;
+//     }
+
+//     counterElements.forEach((element) => {
+//         element.innerText = counter.value.toString();
+//     });
+
+//     resultElement.innerText = result.value.toString();
+// }
+
+// const button = document.querySelector<HTMLButtonElement>("button");
+
+// button?.addEventListener("click", () => {
+//     counter.value++;
+// });
+
+// watch(counter, () => {
+//     setCounter();
+// });
